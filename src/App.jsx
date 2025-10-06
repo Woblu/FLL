032185ca17cdbd0a560d0b8c84ff760c9871aaf7
Woblu <boaz.vega@proton.me>
2025-10-05 22:12:32 -0500

    App.jsx fix

--- src/App.jsx
+++ src/App.jsx
@@ -25,10 +25,9 @@ import sideDeco from "./assets/c9b562fc33dfe9e93230abab38e1ef32.webp";
 export default function App() {
   return (
     <LanguageProvider>
-      {/* Updated the main background and text colors */}
       <div className="relative min-h-screen bg-fllDark text-fllWhite flex flex-col overflow-x-hidden">
         <div 
-          className="hidden lg:block absolute left-0 top-0 h-full w-32 xl:w-48 opacity-10 z-10" // Reduced opacity
+          className="hidden lg:block absolute left-0 top-0 h-full w-32 xl:w-48 opacity-10 z-10"
           style={{ backgroundImage: `url(${sideDeco})`, backgroundRepeat: "repeat-y", backgroundPosition: "0px -1.5rem", transform: "scaleX(-1)" }}
         ></div>
         
@@ -59,12 +58,12 @@ export default function App() {
         </main>
 
         <div 
-          className="hidden lg:block absolute right-0 top-0 h-full w-32 xl:w-48 opacity-10 z-10" // Reduced opacity
+          className="hidden lg:block absolute right-0 top-0 h-full w-32 xl:w-48 opacity-10 z-10"
           style={{ backgroundImage: `url(${sideDeco})`, backgroundRepeat: "repeat-y", backgroundPosition: "0px -1.5rem" }}
         ></div>
         
         <ReloadPrompt />
       </div>
-    </Language-Provider>
+    </LanguageProvider>
   );
 }
